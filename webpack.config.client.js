f9de444d0e929245ebc4283df00628f02287dbdc
Joseph Morse <tamed.lionheart@gmail.com>
2017-10-24 18:07:06 -0400

    fix(webpack): renamed webpack config files
    renamed files to match import/no-extraneous-dependencies
    rule

---
+++ webpack.config.client.js
@@ -0,0 +1,38 @@
+const DynamicCdn = require('dynamic-cdn-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const { DefinePlugin } = require('webpack')
+
+module.exports = {
+  entry: {
+    bundle: './src/client'
+  },
+
+  devtool: process.env.NODE_ENV !== 'production' ? 'cheap-module-source-map' : false,
+
+  output: {
+    path: `${__dirname}/public`,
+    filename: 'js/[name].js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['public/js']),
+    new DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+      }
+    }),
+    new ManifestPlugin(),
+    new DynamicCdn()
+  ]
+}
